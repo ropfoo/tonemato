9022a594cb36f19fdc6cb1a3c6c328189f60603e
RopFoo <ropfoo@gmail.com>
2023-04-08 22:20:05 +0200

    add custom font

--- smeargle/src/root.tsx
+++ smeargle/src/root.tsx
@@ -1,5 +1,5 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { Suspense } from 'solid-js';
 import {
   useLocation,
   A,
@@ -12,32 +12,33 @@ import {
   Routes,
   Scripts,
   Title,
-} from "solid-start";
-import "./root.css";
+} from 'solid-start';
+import './root.css';
+import '../styles/fonts.css';
 
 export default function Root() {
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+      ? 'border-sky-600'
+      : 'border-transparent hover:border-sky-600';
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head>
         <Title>SolidStart - With TailwindCSS</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta charset='utf-8' />
+        <Meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                  <A href="/">Home</A>
+            <nav class='bg-sky-800'>
+              <ul class='container flex items-center p-3 text-gray-200'>
+                <li class={`border-b-2 ${active('/')} mx-1.5 sm:mx-6`}>
+                  <A href='/'>Home</A>
                 </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
+                <li class={`border-b-2 ${active('/about')} mx-1.5 sm:mx-6`}>
+                  <A href='/about'>About</A>
                 </li>
               </ul>
             </nav>
--- smeargle/tailwind.config.cjs
+++ smeargle/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'teaser-gradient-darkblue':
+          'linear-gradient(299.63deg, #3822B3 0%, #333333 47.71%, #3E3E3E 100%)',
+      },
+    },
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
   },
-  plugins: []
+  plugins: [],
 };
