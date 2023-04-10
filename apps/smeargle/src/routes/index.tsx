08f2f2bc03f06098dde6571e2404d96c2d6efff8
RopFoo <ropfoo@gmail.com>
2023-04-10 12:51:35 +0200

    fix resposnse data

--- apps/smeargle/src/routes/index.tsx
+++ apps/smeargle/src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { createResource, Show } from 'solid-js';
-import { isServer } from 'solid-js/web';
-import { useRouteData } from 'solid-start';
-import { Teaser as TeaserType } from 'tonemato-types';
-import Teaser from '~/components/Teaser';
+import { createResource, Show } from "solid-js";
+import { isServer } from "solid-js/web";
+import { useRouteData } from "solid-start";
+import { Teaser as TeaserType } from "tonemato-types";
+import Teaser from "~/components/Teaser";
 
 async function fetchRalts() {
-  console.log('server: ', isServer);
+  console.log("server: ", isServer);
   const response = await fetch(
-    isServer ? 'http://ralts:3005/' : 'http://localhost:3005/'
+    isServer ? "http://ralts:3005/" : "http://localhost:3005/"
   );
   return await response.json();
 }
@@ -19,18 +19,16 @@ export function routeData() {
 export default function Home() {
   const [data] = useRouteData<typeof routeData>();
 
-  const error = <div>something wen wrong</div>;
+  const error = <div>something went wrong</div>;
 
   return (
-    <main class='mx-auto  p-4 font-montserrat'>
-      <p>Hello test</p>
+    <main class="mx-auto  p-4 font-montserrat">
+      <p>Hello</p>
       <Show when={!data.error} fallback={error}>
-        <div class='grid grid-cols-2 gap-6'>
-          {data()?.cache?.backstagepro?.data?.pages[0]?.map(
-            (teaser: TeaserType) => (
-              <Teaser teaser={teaser} />
-            )
-          )}
+        <div class="grid grid-cols-2 gap-6">
+          {data()?.cache?.backstagepro?.pages[0]?.map((teaser: TeaserType) => (
+            <Teaser teaser={teaser} />
+          ))}
         </div>
       </Show>
     </main>
