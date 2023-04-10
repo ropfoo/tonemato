9e1e892fe818a9743a07b05af3468d8544b20a10
RopFoo <ropfoo@gmail.com>
2023-04-10 12:19:33 +0200

    move all projects in apps folder

--- apps/smeargle/src/routes/index.tsx
+++ apps/smeargle/src/routes/index.tsx
@@ -1,7 +1,8 @@
-import { createResource } from 'solid-js';
+import { createResource, Show } from 'solid-js';
 import { isServer } from 'solid-js/web';
 import { useRouteData } from 'solid-start';
-import Teaser, { EntryTeaser } from '~/components/Teaser';
+import { Teaser as TeaserType } from 'tonemato-types';
+import Teaser from '~/components/Teaser';
 
 async function fetchRalts() {
   console.log('server: ', isServer);
@@ -18,13 +19,20 @@ export function routeData() {
 export default function Home() {
   const [data] = useRouteData<typeof routeData>();
 
+  const error = <div>something wen wrong</div>;
+
   return (
     <main class='mx-auto  p-4 font-montserrat'>
-      <div class='grid grid-cols-2 gap-6'>
-        {data()?.cache.backstagepro.data.pages[0].map((teaser: EntryTeaser) => (
-          <Teaser teaser={teaser} />
-        ))}
-      </div>
+      <p>Hello test</p>
+      <Show when={!data.error} fallback={error}>
+        <div class='grid grid-cols-2 gap-6'>
+          {data()?.cache?.backstagepro?.data?.pages[0]?.map(
+            (teaser: TeaserType) => (
+              <Teaser teaser={teaser} />
+            )
+          )}
+        </div>
+      </Show>
     </main>
   );
 }
