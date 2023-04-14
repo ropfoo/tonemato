3baf3efbfdd0ee25f4604f23dc53ee5bf9d3dae0
RopFoo <ropfoo@gmail.com>
2023-04-14 09:09:41 +0200

    remove unused variable + prettier format

--- apps/ralts/src/cache/update-cache.ts
+++ apps/ralts/src/cache/update-cache.ts
@@ -1,28 +1,27 @@
-import axios from "axios";
-import redis from "./redis";
+import axios from 'axios';
+import redis from './redis';
 
 export default async function updateCache() {
   try {
-    const timestamp = Date.now();
-    console.log("fetching data");
+    console.log('fetching data');
     const {
       data: { backstagepro, musicstore, musikersucht },
-    } = await axios.get("http://drilbur:3001/scrape");
+    } = await axios.get('http://drilbur:3001/scrape');
 
-    redis.set("backstagepro", JSON.stringify(backstagepro));
-    console.log("backstagepro Data stored!");
+    redis.set('backstagepro', JSON.stringify(backstagepro));
+    console.log('backstagepro Data stored!');
 
-    redis.set("musicstore", JSON.stringify(musicstore));
-    console.log("musicstore Data stored!");
+    redis.set('musicstore', JSON.stringify(musicstore));
+    console.log('musicstore Data stored!');
 
-    redis.set("musikersucht", JSON.stringify(musikersucht));
-    console.log("Musikersucht Data stored!");
+    redis.set('musikersucht', JSON.stringify(musikersucht));
+    console.log('Musikersucht Data stored!');
 
-    redis.set("timestamp", new Date().toJSON());
+    redis.set('timestamp', new Date().toJSON());
 
-    console.log("done!");
+    console.log('done!');
   } catch (e) {
-    console.error("failed fetching data: ", e);
+    console.error('failed fetching data: ', e);
     throw new Error(e);
   }
 }
