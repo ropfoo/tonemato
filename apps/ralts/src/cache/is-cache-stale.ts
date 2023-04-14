1074b003310775ba6cd01c0750f7a42d662d7163
RopFoo <ropfoo@gmail.com>
2023-04-14 08:12:30 +0200

    add tonemato-utils to ralts

--- apps/ralts/src/cache/is-cache-stale.ts
+++ apps/ralts/src/cache/is-cache-stale.ts
@@ -1,10 +1,14 @@
-import redis from "./redis";
+import { isTimePassed } from 'tonemato-utils';
+import redis from './redis';
 
 export async function isCacheStale() {
   try {
-    const latestTimestamp = await redis.get("timestamp");
-    const oneDayAgo = Date.now() - 1000 * 60 * 60 * 24;
-    return new Date(latestTimestamp).getMilliseconds() > oneDayAgo;
+    const latestTimestamp = await redis.get('timestamp');
+    if (!latestTimestamp) return true;
+    return isTimePassed({
+      date: new Date(latestTimestamp),
+      timeOffset: 'oneDay',
+    });
   } catch (e) {
     console.log(e);
     return true;
