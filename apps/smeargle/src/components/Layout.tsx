514f6eaab0d38c4f86901e29b8c907d232b039ad
RopFoo <ropfoo@gmail.com>
2023-04-17 21:49:45 +0200

    issue-25 fix linting errors
    - mainly solid was mad for not making things reactive
    - prettier stuff

--- apps/smeargle/src/components/Layout.tsx
+++ apps/smeargle/src/components/Layout.tsx
@@ -4,10 +4,10 @@ interface LayoutProps {
   children: JSX.Element;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout(props: LayoutProps) {
   return (
     <div class="text-whinehouse dark:text-snow px-4 pt-[140px] sm:px-8 lg:px-16">
-      {children}
+      {props.children}
     </div>
   );
 }
