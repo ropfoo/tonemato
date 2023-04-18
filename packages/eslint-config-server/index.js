fd14727dee23af992e1ec36b456685703b629cb9
RopFoo <ropfoo@gmail.com>
2023-04-18 18:59:21 +0200

    issue-25 fix config

--- packages/eslint-config-server/index.js
+++ packages/eslint-config-server/index.js
@@ -5,15 +5,9 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended'],
 
   rules: {
     'prettier/prettier': 'warn',
-    // Needs to be turned off because of a strange error probably related to enum
-    // 'no-unused-vars': 'off',
   },
 };
