218d8b5825b4bbb137477c750481fe1b93395c30
Robin Dürhager <robin.duerhager@gmail.com>
2023-04-27 22:29:31 +0200

    fix smeargle to use the new ralts data format

--- apps/smeargle/src/routes/index.tsx
+++ apps/smeargle/src/routes/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
     <main>
       <Show when={!data.error} fallback={error}>
         <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-y-12 lg:grid-cols-3 lg:gap-11 lg:gap-y-16 xl:grid-cols-4">
-          <For each={data()}>
+          <For each={data().teasers}>
             {(teaser: TeaserType) => <Teaser teaser={teaser} />}
           </For>
         </div>
