feccd30b9f7b8a19291e5a11d552853be440f51a
Robert Pasdziernik <ropfoo@gmail.com>
2023-05-09 20:22:49 +0200

    Update apps/smeargle/src/components/Filter/FilterDetailed/useFilterStorage.ts
    Co-authored-by: Robin Dürhager <robin.duerhager@gmail.com>

--- apps/smeargle/src/components/Filter/FilterDetailed/useFilterStorage.ts
+++ apps/smeargle/src/components/Filter/FilterDetailed/useFilterStorage.ts
@@ -10,9 +10,7 @@ export function useFilterStorage() {
 
   const getFilterStorage = () => {
     const filterStorage = localStorage.getItem('filter');
-    return filterStorage
-      ? (JSON.parse(filterStorage) as FilterState['filter'])
-      : null;
+    return filterStorage && (JSON.parse(filterStorage) as FilterState['filter']);
   };
 
   return { storeFilterState, getFilterStorage };
