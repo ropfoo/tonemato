866cd8a0e633daf25fc789244c881ba0b57c4817
RopFoo <ropfoo@gmail.com>
2023-05-11 14:10:34 +0200

    init Teaser type and scrape first values

--- apps/drilbur/internal/scrape/scrape.go
+++ apps/drilbur/internal/scrape/scrape.go
@@ -6,12 +6,41 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Teaser struct {
+	Url             string
+	Date            string
+	Title           string
+	Description     string
+	ZipCode         string
+	City            string
+	PreviewImageUrl string
+	Domain          string
+}
+
 func Start() string {
 	fmt.Println("Start the scraping fun!")
 	collector := colly.NewCollector()
 
-	collector.OnHTML(".teaser-body h4", func(element *colly.HTMLElement) {
-		fmt.Println(element.Text)
+	teasers := make([]Teaser, 0)
+
+	collector.OnHTML(".teaser", func(element *colly.HTMLElement) {
+
+		var newTeaser Teaser
+
+		element.ForEach(".teaser-body h4", func(index int, titleElement *colly.HTMLElement) {
+			newTeaser.Title = titleElement.Text
+		})
+
+		element.ForEach(".teaser-text", func(index int, titleElement *colly.HTMLElement) {
+			newTeaser.Description = titleElement.Text
+		})
+
+		teasers = append(teasers, newTeaser)
+
+	})
+
+	collector.OnScraped(func(r *colly.Response) {
+		fmt.Println("yay its done!!", teasers)
 	})
 
 	collector.Visit("http://localhost:8080/mock")
