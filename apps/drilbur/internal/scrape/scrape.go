8235c2639d1d67b0a4982fd94d518fae993b20b9
RopFoo <ropfoo@gmail.com>
2023-05-17 17:28:55 +0200

    fix teasers of first page not getting scraped when getting initial page count

--- apps/drilbur/internal/scrape/scrape.go
+++ apps/drilbur/internal/scrape/scrape.go
@@ -31,47 +31,57 @@ var YEAR_THRESHOLD = time.Now().Year() - 3
 func scrapeTeasers(scraper TeaserScraper, channel chan []model.Teaser) {
 	collector := colly.NewCollector()
 	config := scraper.config()
+	firstPageTeasers := make([]model.Teaser, 0)
 	teasers := make([]model.Teaser, 0)
 	pageCount := 1
+
+	// channel for all teasers getting scraped in go routines
 	collectChannel := make(chan []model.Teaser)
-
-	// get page count
-	// TODO: scrape teasers of first page to avoid second visit
-	if pageCount == 1 {
-		collector.OnHTML(config.PageCountTarget, func(element *colly.HTMLElement) {
-			pageCount = scraper.scrapePageCount(element)
-		})
-		collector.Visit(config.Url)
-	}
-
 	// create function to scrape all teasers on each page
 	collectTeasers := func(page int, wg *sync.WaitGroup) {
 		defer wg.Done()
+
+		// DEBUG ONLY: artificial delay
+		time.Sleep(time.Second)
+
 		var collectedTeasers []model.Teaser
 		collector.OnHTML(config.TeaserTarget, func(element *colly.HTMLElement) {
 			newTeaser := scraper.scrapeTeaser(element)
 			// append teaser if it passes year threshold
 			if newTeaser.Date.Year() > YEAR_THRESHOLD {
 				collectedTeasers = append(collectedTeasers, newTeaser)
+				// on first page - store teasers in a separete slice
+				// this is necessary since the first page is not part of a go routine
+				if pageCount == 1 {
+					firstPageTeasers = append(firstPageTeasers, newTeaser)
+				}
 			}
 		})
-
-		// DEBUG ONLY: artificial delay
-		time.Sleep(time.Second)
-
+		// get page count on first scrape
+		if pageCount == 1 {
+			collector.OnHTML(config.PageCountTarget, func(element *colly.HTMLElement) {
+				pageCount = scraper.scrapePageCount(element)
+			})
+		}
 		// go to page url
 		collector.Visit(config.Url + "?pageCount=" + fmt.Sprint(page))
 		// add collectedTeasers to channel
 		collectChannel <- collectedTeasers
-
 	}
 
 	// create go routine to scrape pages concurrently
 	go func() {
 		waitGroup := sync.WaitGroup{}
-		for page := 1; page < pageCount; page++ {
-			waitGroup.Add(1)
-			go collectTeasers(page, &waitGroup)
+		waitGroup.Add(1)
+		// get page count and collect teasers of first page
+		collectTeasers(1, &waitGroup)
+
+		// iterate through all pages left in different go routines
+		if pageCount > 1 {
+			for page := 2; page <= pageCount; page++ {
+				waitGroup.Add(1)
+				go collectTeasers(page, &waitGroup)
+			}
 		}
 		waitGroup.Wait()
 		close(collectChannel)
@@ -80,5 +90,8 @@ func scrapeTeasers(scraper TeaserScraper, channel chan []model.Teaser) {
 	for chanTeasers := range collectChannel {
 		teasers = chanTeasers
 	}
+	// add teasers of the first page to the teasers recieved by the collectChannel
+	teasers = append(teasers, firstPageTeasers...)
+	// return all teasers to the main channel
 	channel <- teasers
 }
